301849a947c7017ee974058237381b952408c402
Jared Collier <collierjared@gmail.com>
2023-11-28 11:29:37 -0700

    initial commit: strings file done

---
+++ strings.js
@@ -0,0 +1,14 @@
+const hello = "Hello, world!"
+let stringWithSingleQuotes = 'Single quotes work too'
+
+console.log(hello[0])
+
+const ball = 'ball'
+const templateString = `foot${ball}`
+
+console.log(templateString)
+
+let yourName = 'Steve'
+console.log(`Hi, ${yourName}!`)
+
+console.log("Hi, " + yourName + "!")
